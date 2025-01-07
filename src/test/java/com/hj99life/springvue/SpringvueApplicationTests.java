735641e1ea1e73c0106153e400d9ad84411feb47
hj99life <hj99life@gmail.com>
2025-01-07 11:39:58 +0900

    first commit

---
+++ src/test/java/com/hj99life/springvue/SpringvueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hj99life.springvue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringvueApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
